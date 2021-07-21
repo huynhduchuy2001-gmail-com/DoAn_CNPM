3c243a32adf41514cd2a5ad5da2227e13e0bc051
huỳnh đức huy <huynhduchuy2001@gmail.com>
2021-07-21 19:18:55 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,32 @@
+/* eslint-disable array-callback-return */
+import './App.css';
+import JSONDATA from './MOCK_DATA.json'
+import {useState} from 'react'
+const App = () =>{
+  const [searchTerm, setSearchTerm] = useState("")
+  return(
+    <div className = "App">
+      <input type = "text" 
+      placeholder = "Search..." 
+      onChange = {(event) => {
+        setSearchTerm(event.target.value);
+      }}/>
+      {JSONDATA.filter((val) => {
+        // eslint-disable-next-line eqeqeq
+        if(searchTerm == ""){
+          return val
+        }
+        else if(val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+          return val
+        }
+      }).map((val,key) =>{
+        return (
+          <div className = "user" key = {key}> 
+            <p>{val.first_name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export default App;
